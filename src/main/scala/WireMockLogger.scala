8e0b28827591b8cb0c93efb979cc3277a40872e9
Darren Ballinger <darren.ballinger@gmail.com>
2015-08-27 18:31:58 +0100

    Added url and method stub messages

--- src/main/scala/WireMockLogger.scala
+++ src/main/scala/WireMockLogger.scala
@@ -1,17 +1,41 @@
 import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.matching.RequestPattern
 import org.scalatest.{Failed, Suite}
 
 trait WireMockLogger {
   this: Suite =>
 
+  import collection.JavaConversions._
+
   def wiremockFailureLog = System.out
 
-  def mockServers:Iterable[MockServer]
+  def mockServers: Iterable[MockServer]
+
+  def printExpectedAndActualRequests(test: NoArgTest) = {
+    wiremockFailureLog.println(s"Wiremock expected and actual requests for failing test '${test.name}'")
+    mockServers foreach {
+      ms =>
+        val server = ms.server
+        val mappings = server.listAllStubMappings().getMappings
+        if (mappings.isEmpty)
+          wiremockFailureLog.println(s"Wiremock server '${ms.name}' did not expect any requests. Did you forget to configure any stubs?")
+        else
+          wiremockFailureLog.println(s"Wiremock server '${ms.name}' stub mappings:")
+        mappings foreach {
+          mapping =>
+            val pattern = mapping.getRequest
+            pattern match {
+              case UrlMatcher(url) => wiremockFailureLog.println(s"${pattern.getMethod.value()}: ${url.description}")
+              case _ =>
+            }
+        }
+    }
+  }
 
   override protected def withFixture(test: NoArgTest) = {
     test() match {
       case outcome: Failed =>
-        wiremockFailureLog.println(s"Ooops... ${test.name}")
+        printExpectedAndActualRequests(test)
         outcome
       case outcome =>
         outcome
@@ -20,4 +44,18 @@ trait WireMockLogger {
 
 }
 
-case class MockServer(name:String, server:WireMockServer)
+case class MockServer(name: String, server: WireMockServer)
+
+class UrlMatcher(urlOrPath: String, policy: String, url: String) {
+  def description = s"$urlOrPath $policy $url"
+}
+
+object UrlMatcher {
+  def unapply(req: RequestPattern): Option[UrlMatcher] = (req.getUrl, req.getUrlPath, req.getUrlPattern, req.getUrlPathPattern) match {
+    case (url, null, null, null) => Option(new UrlMatcher("URL", "==", url))
+    case (null, path, null, null) => Option(new UrlMatcher("Path", "==", path))
+    case (null, null, urlPattern, null) => Option(new UrlMatcher("URL", "~=", urlPattern))
+    case (null, null, null, pathPattern) => Option(new UrlMatcher("Path", "~=", pathPattern))
+    case _ => None
+  }
+}
--- src/test/scala/WireMockLoggerTest.scala
+++ src/test/scala/WireMockLoggerTest.scala
@@ -13,31 +13,80 @@ class WireMockLoggerTest extends WordSpec with Matchers with BeforeAndAfterEach 
   import com.github.tomakehurst.wiremock.client.WireMock._
 
   trait Fixture extends FunSpecLike with WireMockLogger {
-    var mockServer = new WireMockServer(0)
-    mockServer.start()
-    val mockServers = mockServer :: Nil
+    var mockServer1 = MockServer("server1", new WireMockServer(0))
+    var mockServer2 = MockServer("server2", new WireMockServer(0))
+    val mockServers = mockServer1 :: mockServer2 :: Nil
+    mockServers foreach {_.server.start()}
+
+    def stopStubbing() = mockServers foreach {_.server.stop()}
 
     val logCs: Charset = Charsets.UTF_8
     val out = new ByteArrayOutputStream()
     override val wiremockFailureLog: PrintStream = new PrintStream(out, true, logCs.name())
 
-    val failedTest = Failed(new Exception)
+    val passingTest = new NoArgTest {
+      override def apply(): Outcome = Succeeded
+
+      override val text: String = "it passed"
+      override val configMap: ConfigMap = new ConfigMap(Map.empty[String, Any])
+      override val scopes: IndexedSeq[String] = IndexedSeq.empty[String]
+      override val name: String = "pass"
+      override val tags: Set[String] = Set.empty[String]
+    }
+    val failingTest = new NoArgTest {
+      override def apply(): Outcome = Failed(new Exception)
+
+      override val text: String = "it failed"
+      override val configMap: ConfigMap = new ConfigMap(Map.empty[String, Any])
+      override val scopes: IndexedSeq[String] = IndexedSeq.empty[String]
+      override val name: String = "fail"
+      override val tags: Set[String] = Set.empty[String]
+    }
 
     def log = Source.fromBytes(out.toByteArray, logCs.name()).getLines().toList
   }
 
-  "" in new Fixture {
-    mockServer.stubFor(get(urlEqualTo("/path")).willReturn(aResponse().withStatus(200)))
-    withFixture(new NoArgTest {
-      override def apply(): Outcome = ???
+  "A passing test should not produce any stubbing log" in new Fixture {
+    //    server1.stubFor(get(urlEqualTo("/path")).willReturn(aResponse().withStatus(200)))
+    withFixture(passingTest)
+    log shouldBe Nil
+    stopStubbing()
+  }
 
-      override val text: String = _
-      override val configMap: ConfigMap = _
-      override val scopes: IndexedSeq[String] = _
-      override val name: String = _
-      override val tags: Set[String] = _
-    })
-//    withFixture(new Failed(new Exception))
-    mockServer.stop()
+  "A failing test should print the name of the test" in new Fixture {
+    //    server1.stubFor(get(urlEqualTo("/path")).willReturn(aResponse().withStatus(200)))
+    withFixture(failingTest)
+    log.head shouldBe s"Wiremock expected and actual requests for failing test '${failingTest.name}'"
+    stopStubbing()
+  }
+
+  "A failing test using an unstubbed wiremock server should explain that there were no expected stubbings" in new Fixture {
+    withFixture(failingTest)
+    log should contain(s"Wiremock server '${mockServer1.name}' did not expect any requests. Did you forget to configure any stubs?")
+    log should contain(s"Wiremock server '${mockServer2.name}' did not expect any requests. Did you forget to configure any stubs?")
+    stopStubbing()
+  }
+
+  "A failing test using a wiremock server with multiple stubs should print the expected method and url matchers" in new Fixture {
+    val getUrl = "/url1"
+    val postPath = "/url2"
+    val putUrlMatch = "/url3"
+    val deletePathMatch = "/url4"
+
+    mockServer1.server.stubFor(get(urlEqualTo(getUrl)).willReturn(aResponse().withStatus(200)))
+    mockServer1.server.stubFor(post(urlPathEqualTo(postPath)).willReturn(aResponse().withStatus(200)))
+    mockServer1.server.stubFor(put(urlMatching(putUrlMatch)).willReturn(aResponse().withStatus(200)))
+    mockServer1.server.stubFor(delete(urlPathMatching(deletePathMatch)).willReturn(aResponse().withStatus(200)))
+    withFixture(failingTest)
+
+    val server1Log = log.takeWhile {!_.contains(mockServer2.name)}
+
+    server1Log should contain(s"Wiremock server '${mockServer1.name}' stub mappings:")
+    server1Log should contain(s"GET: URL == $getUrl")
+    server1Log should contain(s"POST: Path == $postPath")
+    server1Log should contain(s"PUT: URL ~= $putUrlMatch")
+    server1Log should contain(s"DELETE: Path ~= $deletePathMatch")
+
+    stopStubbing()
   }
 }
